06e396a359ff59cf43b00ea86fa7e16b986a303e
Chris Burns <burns.christopherd@gmail.com>
2026-07-16 21:01:41 -0400

    Wire enhanced runtime services

--- src/runtime.js
+++ src/runtime.js
@@ -1,8 +1,10 @@
 import { loadConfig } from './config.js';
+import { createResponder } from './personality.js';
 import { PlexClient } from './plex-client.js';
 import { QueueStore } from './queue-store.js';
 
 export const config = loadConfig();
+export const respond = createResponder({ mode: config.personalityMode });
 export const plex = new PlexClient({
   baseUrl: config.plexUrl,
   token: config.plexToken,
@@ -11,6 +13,10 @@ export const plex = new PlexClient({
   maxTracks: config.maxQueueTracks,
   maxAudioBitrate: config.maxAudioBitrate,
   transcodePolicy: config.transcodePolicy,
-  requestTimeoutMs: config.plexRequestTimeoutMs
+  requestTimeoutMs: config.plexRequestTimeoutMs,
+  lyricsMode: config.lyricsMode,
+  lyricsRequestTimeoutMs: config.lyricsRequestTimeoutMs,
+  radioTrackLimit: config.radioTrackLimit,
+  allowPlaylistWrites: config.allowPlaylistWrites
 });
 export const queueStore = new QueueStore({ tableName: config.queueTable });
